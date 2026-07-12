640151440f1daf531d7779e1d82896b3ed63965e
Trần Hoàng Long <thlong190705@gmail.com>
2026-07-12 15:08:49 +0700

    fix(notification): correct spring.mail YAML nesting so JavaMailSender bean is always created

--- backend-springboot/src/main/java/com/travinh/realty/modules/notification/SmtpEmailSender.java
+++ backend-springboot/src/main/java/com/travinh/realty/modules/notification/SmtpEmailSender.java
@@ -1,14 +1,10 @@
 package com.travinh.realty.modules.notification;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConditionalOnBean(JavaMailSender.class)
-@ConditionalOnProperty(name = "spring.mail.username", matchIfMissing = false)
 public class SmtpEmailSender implements EmailSender {
     private final JavaMailSender mailSender;
 
